204a2b85d7ac4781aed0f97d77521f1162468d17
Mark Smith <mark.alex.smith@hotmail.co.uk>
2025-04-20 23:02:36 +0100

    Improved css

--- obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15845472e195fa6dc54d1f77d9fbaecab1b2eda0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62886304efa6e6c39d0ea68cb6e7aae5d825e95e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
