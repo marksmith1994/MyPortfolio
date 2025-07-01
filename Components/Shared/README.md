fb02e492c62d0f50e3eccdadecaa16afc149ce03
Mark Smith <mark.alex.smith@hotmail.co.uk>
2025-07-01 21:30:03 +0100

    Updated mobile nav and file structure

--- Components/Shared/README.md
+++
@@ -1,3 +0,0 @@
-# Shared Components
-
-Place reusable Blazor components here (e.g., buttons, cards, alerts) that are used across multiple pages or layouts. 
--- obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96db0e91b1525a51903dfbdeb9e0dc515ff802e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb12ae9d895498569fe50c361767eea82596d8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
